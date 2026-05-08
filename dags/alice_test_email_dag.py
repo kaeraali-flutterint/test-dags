7df4324b316824821bc35d7ed3f15f3535e958d4
alicekaerast <alice.kaerast@flutter.com>
2026-05-08 12:53:47 +0100

    fix: remove unneccessary task

--- dags/alice_test_email_dag.py
+++ dags/alice_test_email_dag.py
@@ -1,7 +1,6 @@
 import os
 
 from airflow.providers.smtp.operators.smtp import EmailOperator
-from airflow.providers.standard.operators.python import PythonOperator
 from airflow.sdk import dag
 from pendulum import datetime
 
@@ -15,10 +14,6 @@ default_args = {
 }
 
 
-def hello_world(x):
-    return "Hello " + x
-
-
 @dag(
     dag_id=dag_id,
     default_args=default_args,
@@ -29,11 +24,6 @@ def hello_world(x):
 )
 def alice_test_email_dag():
     print("hi alice")
-    hello = PythonOperator(
-        task_id="hello",
-        python_callable=hello_world,
-        op_kwargs={"x": "world"},
-    )
     email_alice = EmailOperator(
         task_id="email_alice",
         to="alice@example.com",
@@ -41,7 +31,7 @@ def alice_test_email_dag():
         html_content="hello alice",
     )
 
-    hello.set_downstream(email_alice)
+    email_alice
 
 
 alice_test_email_dag()
